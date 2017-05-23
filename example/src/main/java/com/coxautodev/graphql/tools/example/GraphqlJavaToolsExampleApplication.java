2546d7dc4c8c63ddaadc63a873716303ef493c98
Andrew Potter <apottere@gmail.com>
2017-05-23 17:22:20 -0400

    Update sample application to latest version.

--- example/src/main/java/com/coxautodev/graphql/tools/example/GraphqlJavaToolsExampleApplication.java
+++ example/src/main/java/com/coxautodev/graphql/tools/example/GraphqlJavaToolsExampleApplication.java
@@ -1,20 +1,12 @@
 package com.coxautodev.graphql.tools.example;
 
-import com.coxautodev.graphql.tools.GraphQLResolver;
-import com.coxautodev.graphql.tools.SchemaParser;
 import com.coxautodev.graphql.tools.example.types.Droid;
 import com.coxautodev.graphql.tools.example.types.Episode;
 import com.coxautodev.graphql.tools.example.types.Human;
-import graphql.execution.SimpleExecutionStrategy;
-import graphql.schema.GraphQLSchema;
-import graphql.servlet.SimpleGraphQLServlet;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.oembedler.moon.graphql.boot.SchemaParserDictionary;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
-
-import java.util.List;
 
 @SpringBootApplication
 public class GraphqlJavaToolsExampleApplication {
@@ -23,21 +15,9 @@ public class GraphqlJavaToolsExampleApplication {
         SpringApplication.run(GraphqlJavaToolsExampleApplication.class, args);
     }
 
-    @Autowired
-    private List<GraphQLResolver<?>> resolvers;
-
     @Bean
-    public GraphQLSchema graphQLSchema() {
-        return SchemaParser.newParser()
-            .file("swapi.graphqls")
-            .resolvers(resolvers)
-            .dictionary(Human.class, Droid.class, Episode.class)
-            .build()
-            .makeExecutableSchema();
-    }
-
-    @Bean
-    ServletRegistrationBean graphQLServlet(GraphQLSchema graphQLSchema) {
-        return new ServletRegistrationBean(new SimpleGraphQLServlet(graphQLSchema, new SimpleExecutionStrategy()), "/graphql/*");
+    public SchemaParserDictionary schemaParserDictionary() {
+        return new SchemaParserDictionary()
+            .dictionary(Human.class, Droid.class, Episode.class);
     }
 }
