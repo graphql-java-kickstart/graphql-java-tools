0e394d4859d1de4dff163a329c9e31d797a289fb
Michiel Oliemans <oliemans@scopisto.com>
2018-10-03 19:46:04 +0200

    Update RelayConnectionTest with generics #114

--- src/test/java/com/coxautodev/graphql/tools/RelayConnectionTest.java
+++ src/test/java/com/coxautodev/graphql/tools/RelayConnectionTest.java
@@ -1,12 +1,11 @@
 package com.coxautodev.graphql.tools;
 
-import graphql.relay.*;
+import graphql.relay.Connection;
+import graphql.relay.SimpleListConnection;
 import graphql.schema.DataFetchingEnvironment;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class RelayConnectionTest {
 
@@ -21,36 +20,9 @@ public class RelayConnectionTest {
 
     static class QueryResolver implements GraphQLQueryResolver {
         // fixme #114: desired return type to use: Connection<User>
-        public UserConnection users(int first, String after, DataFetchingEnvironment env) {
-            return (UserConnection) new SimpleListConnection<User>(new ArrayList()).get(env);
+        public Connection<User> users(int first, String after, DataFetchingEnvironment env) {
+            return new SimpleListConnection<User>(new ArrayList<>()).get(env);
         }
-    }
-
-    // fixme #114: remove this implementation
-    static class UserConnection extends DefaultConnection<User> {
-
-        UserConnection(List<Edge<User>> edges, PageInfo pageInfo) {
-            super(edges, pageInfo);
-        }
-
-        public List<UserEdge> edges() {
-            return super.getEdges().stream()
-                    .map(UserEdge.class::cast)
-                    .collect(Collectors.toList());
-        }
-
-        public PageInfo getPageInfo() {
-            return super.getPageInfo();
-        }
-    }
-
-    // fixme #114: remove this implementation
-    static class UserEdge extends DefaultEdge<User> {
-
-        UserEdge(User node, ConnectionCursor cursor) {
-            super(node, cursor);
-        }
-
     }
 
     static class User {
