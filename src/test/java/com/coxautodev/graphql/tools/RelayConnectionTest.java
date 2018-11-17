04e2733ae823813a1cd73afc84760e1c4e989335
Michiel Oliemans <oliemans@scopisto.com>
2018-11-17 14:03:25 +0100

    Cleaned up unit test

--- src/test/java/com/coxautodev/graphql/tools/RelayConnectionTest.java
+++ src/test/java/com/coxautodev/graphql/tools/RelayConnectionTest.java
@@ -4,30 +4,22 @@ import graphql.GraphQL;
 import graphql.execution.AsyncExecutionStrategy;
 import graphql.relay.Connection;
 import graphql.relay.SimpleListConnection;
-import graphql.schema.*;
-import graphql.schema.idl.SchemaDirectiveWiring;
-import graphql.schema.idl.SchemaDirectiveWiringEnvironment;
+import graphql.schema.DataFetchingEnvironment;
+import graphql.schema.GraphQLSchema;
 import groovy.lang.Closure;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RelayConnectionTest {
 
-    private static final Logger log = LoggerFactory.getLogger(RelayConnectionTest.class);
-
     @Test
     public void compiles() {
-        SchemaParserOptions options = SchemaParserOptions.newOptions()
-
-                .build();
         GraphQLSchema schema = SchemaParser.newParser().file("RelayConnection.graphqls")
                 .resolvers(new QueryResolver())
                 .dictionary(User.class)
-                .directive("connection", new ConnectionDirective())
-                .options(options)
                 .build()
                 .makeExecutableSchema();
 
@@ -35,12 +27,12 @@ public class RelayConnectionTest {
                 .queryExecutionStrategy(new AsyncExecutionStrategy())
                 .build();
 
-        Map<String,Object> variables = new HashMap<>();
+        Map<String, Object> variables = new HashMap<>();
         variables.put("limit", 10);
         Utils.assertNoGraphQlErrors(gql, variables, new Closure<String>(null) {
             @Override
             public String call() {
-                return  "query {\n" +
+                return "query {\n" +
                         "   users {\n" +
                         "       edges {\n" +
                         "           cursor\n" +
@@ -60,7 +52,6 @@ public class RelayConnectionTest {
     }
 
     static class QueryResolver implements GraphQLQueryResolver {
-        // fixme #114: desired return type to use: Connection<User>
         public Connection<User> users(int first, String after, DataFetchingEnvironment env) {
             return new SimpleListConnection<>(Collections.singletonList(new User(1L, "Luke"))).get(env);
         }
@@ -74,17 +65,6 @@ public class RelayConnectionTest {
             this.id = id;
             this.name = name;
         }
-    }
-
-    static class ConnectionDirective implements SchemaDirectiveWiring {
-
-        @Override
-        public GraphQLFieldDefinition onField(SchemaDirectiveWiringEnvironment<GraphQLFieldDefinition> environment) {
-            GraphQLFieldDefinition field = environment.getElement();
-            log.info("Transforming field");
-            return field;
-        }
-
     }
 
 
