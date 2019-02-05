305d630e2a32d283bb4b58a333fa44ac687ba889
Jonathan Haber <jhaber@hubspot.com>
2019-02-04 21:25:25 -0500

    Use ClassMate to find map type arguments

--- src/main/kotlin/com/coxautodev/graphql/tools/PropertyMapResolver.kt
+++ src/main/kotlin/com/coxautodev/graphql/tools/PropertyMapResolver.kt
@@ -1,9 +1,9 @@
 package com.coxautodev.graphql.tools
 
+import com.fasterxml.classmate.TypeResolver
 import graphql.language.FieldDefinition
 import graphql.schema.DataFetcher
 import graphql.schema.DataFetchingEnvironment
-import java.lang.reflect.ParameterizedType
 
 /**
  * @author Nick Weedon
@@ -14,10 +14,23 @@ internal class PropertyMapResolver(field: FieldDefinition, search: FieldResolver
 
     var mapGenericValue : JavaType = getMapGenericType(relativeTo)
 
+    /**
+     * this impl still has some problems:
+     * - mapClass lost its generics somewhere along the way, so if you had
+     *   something like {@code class Resolver<V> implements Map<String, V>}
+     *   then we won't be able to infer the value type
+     * - this doesn't handle the case of the map value type being generic,
+     *   for example {@code class Resolver implements Map<String, MyType<MyTypeArg>>}
+     */
     fun getMapGenericType(mapClass : JavaType) : JavaType {
-        if(mapClass is ParameterizedType) {
-            return mapClass.actualTypeArguments[1]
+        val resolvedType = TypeResolver().resolve(mapClass)
+
+        val mapValueType = resolvedType.typeParametersFor(Map::class.java)[1]
+        if (mapValueType.typeParameters.isEmpty()) {
+            // no type params should mean regular class
+            return mapValueType.erasedType
         } else {
+            // TODO raise an error? try to handle this case?
             return Object::class.java
         }
     }
