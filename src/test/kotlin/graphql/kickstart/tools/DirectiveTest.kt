70282138d4a2510898d43ea370eceb369f8bde55
Vojtech Polivka <vojta.polivka@gmail.com>
2020-07-02 16:10:59 -0400

    Add test for enums in directives

---
+++ src/test/kotlin/graphql/kickstart/tools/DirectiveTest.kt
@@ -0,0 +1,68 @@
+package graphql.kickstart.tools
+
+import graphql.GraphQL
+import graphql.execution.AsyncExecutionStrategy
+import graphql.schema.GraphQLFieldDefinition
+import graphql.schema.idl.SchemaDirectiveWiring
+import graphql.schema.idl.SchemaDirectiveWiringEnvironment
+import org.junit.Ignore
+import org.junit.Test
+
+class DirectiveTest {
+
+    @Test
+    @Ignore("Ignore until enums work in directives")
+    fun `should compile schema with directive that has enum parameter`() {
+        val schema = SchemaParser.newParser().schemaString("""
+              directive @allowed(state: [AllowedState!]) on FIELD_DEFINITION
+              
+              enum AllowedState {
+                  ALLOWED
+                  DISALLOWED
+              }
+              
+              type Book {
+                  id: Int!
+                  name: String! @allowed(state: [ALLOWED])
+              }
+              
+              type Query {
+                  books: [Book!]
+              }
+            """)
+            .resolvers(QueryResolver())
+            .directive("allowed", AllowedDirective())
+            .build()
+            .makeExecutableSchema()
+
+        GraphQL.newGraphQL(schema)
+            .queryExecutionStrategy(AsyncExecutionStrategy())
+            .build()
+    }
+
+    class QueryResolver : GraphQLQueryResolver {
+        fun books(): List<Book> {
+            return listOf(Book(42L, "Test Book"))
+        }
+    }
+
+    data class Book(
+        val id: Long,
+        val name: String
+    )
+
+    enum class AllowedState {
+        ALLOWED,
+        DISALLOWED
+    }
+
+    class AllowedDirective : SchemaDirectiveWiring {
+        override fun onField(environment: SchemaDirectiveWiringEnvironment<GraphQLFieldDefinition>): GraphQLFieldDefinition {
+            val field = environment.element
+
+            // TODO
+
+            return field
+        }
+    }
+}
