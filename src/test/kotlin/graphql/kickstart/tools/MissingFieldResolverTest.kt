f04cc20919f7becf57335207f9d825b06ad4e295
Tim Ward <timward60@gmail.com>
2020-11-14 15:00:13 -0800

    Add basic test for missing field handling.
    Remove cast from handler.

---
+++ src/test/kotlin/graphql/kickstart/tools/MissingFieldResolverTest.kt
@@ -0,0 +1,75 @@
+package graphql.kickstart.tools
+
+import graphql.ExecutionInput
+import graphql.GraphQL
+import graphql.kickstart.tools.resolver.FieldResolverError
+import graphql.schema.DataFetchingEnvironment
+import org.junit.Assert
+import org.junit.Test
+import java.util.*
+
+class MissingFieldResolverTest {
+
+    @Test(expected = FieldResolverError::class)
+    fun `should throw error`() {
+        SchemaParser.newParser()
+                .schemaString("""
+                    type Query {
+                        implementedField(input: String): String
+                        missingField(input: Int): Int
+                    }
+                    """
+                )
+                .resolvers(object : GraphQLQueryResolver {
+                    fun implementedField(input: Optional<String>) = input.toString()
+                })
+                .build()
+                .makeExecutableSchema()
+    }
+
+    @Test
+    fun `should call missing field resolver handler if provided`() {
+        val schema = SchemaParser.newParser()
+                .schemaString("""
+                    type Query {
+                        implementedField(input: String): String
+                        missingField(input: Int): Int
+                    }
+                    """
+                )
+                .resolvers(object : GraphQLQueryResolver {
+                    fun implementedField(input: Optional<String>) = input.toString()
+                })
+                .options(SchemaParserOptions.newOptions()
+                        .missingFieldHandler(TestMissingFieldHandler())
+                        .build())
+                .build()
+                .makeExecutableSchema()
+
+        val gql = GraphQL.newGraphQL(schema).build()
+
+        val result = gql
+                .execute(ExecutionInput.newExecutionInput()
+                        .query("""
+                            query {
+                                implementedField(input: "test-value")
+                                missingField(input: 1)
+                            }
+                            """)
+                        .context(Object())
+                        .root(Object()))
+
+        val expected = mapOf(
+                "implementedField" to "Optional[test-value]",
+                "missingField" to 1
+        )
+
+        Assert.assertEquals(expected, result.getData())
+    }
+
+    class TestMissingFieldHandler: MissingFieldHandler {
+        override fun resolve(env: DataFetchingEnvironment?): Any? {
+            return env?.getArgument("input");
+        }
+    }
+}
